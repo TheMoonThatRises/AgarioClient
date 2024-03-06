6a0f99ef01bf9a2d4ae6eefbe9c31042e6279975
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-03-05 22:04:51 -0700

    add coords to infographic

--- src/ceccs/game/panes/Overlay.java
+++ src/ceccs/game/panes/Overlay.java
@@ -12,9 +12,10 @@ import javafx.scene.layout.VBox;
 
 public class Overlay extends BorderPane {
 
-    Label fps;
-    Label mass;
-    Label ping;
+    final private Label fps;
+    final private Label mass;
+    final private Label ping;
+    final private Label coordinate;
 
     public Overlay(Player player) {
         super.setPadding(new Insets(10));
@@ -22,16 +23,18 @@ public class Overlay extends BorderPane {
         this.fps = new Label("fps:");
         this.mass = new Label("mass:");
         this.ping = new Label("ping:");
+        this.coordinate = new Label("coords:");
 
         this.mass.setLayoutY(Client.screenHeight);
         this.mass.textProperty().bind(player.massProperty().asString("mass: %.2f"));
 
         Client.heartbeat.addRoutine(now -> {
-            fps.setText(String.format("fps: %.2f", Client.heartbeat.getFramerate()));
-            ping.setText(String.format("ping: %.2fms", NetworkHandler.getPing() / 1_000_000.0));
+            this.fps.setText(String.format("fps: %.2f", Client.heartbeat.getFramerate()));
+            this.ping.setText(String.format("ping: %.2fms", NetworkHandler.getPing() / 1_000_000.0));
+            this.coordinate.setText(String.format("coords: %d, %d", (int) player.getX(), (int) player.getX()));
         });
 
-        VBox infographics = new VBox(5, this.fps, this.mass, this.ping);
+        VBox infographics = new VBox(5, this.fps, this.mass, this.ping, this.coordinate);
         HBox mapLayout = new HBox(new Map(player));
         mapLayout.setAlignment(Pos.BASELINE_RIGHT);
 
