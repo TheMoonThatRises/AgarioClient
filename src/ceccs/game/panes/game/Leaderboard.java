f88bdd2c5fcfb290ac05d68e52d2e7124b6a05a3
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-04-15 20:39:20 -0600

    change dir name utilities -> utils

--- src/ceccs/game/panes/game/Leaderboard.java
+++ src/ceccs/game/panes/game/Leaderboard.java
@@ -2,7 +2,7 @@ package ceccs.game.panes.game;
 
 import ceccs.Client;
 import ceccs.game.objects.ui.Player;
-import ceccs.game.utilities.Utilities;
+import ceccs.game.utils.Utilities;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
